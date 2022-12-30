ce8528f5e31a101be4405b0bce53ce8b08d61c6c
grahamthedevrel <graham:grahamthe.dev>
2022-12-30 13:26:58 +0000

    Initial Commit just to test setup is correct

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Graham The Dev</title>
+
+<!-- Stylesheet -->
+<link rel="stylesheet" type="text/css" href="https://web.archive.org/web/20160223144859cs_/http://www.netdec.com/wp-content/themes/netdec/css/bootstrap.min.css"/>
+<link rel="shortcut icon" href="https://web.archive.org/web/20160223144859im_/http://www.netdec.com/wp-content/themes/netdec/img/favicon.ico"/>
+</head>
+<body>
+
+
+<!-- Scripts --> 
+<script type="text/javascript" src="https://web.archive.org/web/20160223144859js_/http://www.netdec.com/wp-content/themes/netdec/js/newsite/external.js"></script> 
+
+
+</body>
+</html>
